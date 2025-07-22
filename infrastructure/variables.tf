970e6244aa532d6834d88711f806e01d45b2caf5
Manoj-14 <manumadhu1425@gmail.com>
2025-07-22 22:57:53 +0530

    test: updated to test workflow

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -13,7 +13,7 @@ variable "availability_zones" {
 variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
-  description = "CIDR for vpc"
+  description = "cidr for vpc"
 }
 
 variable "public_subnet_cidrs" {
