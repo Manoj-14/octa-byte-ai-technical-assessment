2ee832079508a1f4b8e83994e0c80f16f026f66b
Manoj-14 <manumadhu1425@gmail.com>
2025-07-22 22:54:29 +0530

    updated tf code

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -13,7 +13,7 @@ variable "availability_zones" {
 variable "vpc_cidr" {
   type        = string
   default     = "10.0.0.0/16"
-  description = "cidr for vpc"
+  description = "CIDR for vpc"
 }
 
 variable "public_subnet_cidrs" {
